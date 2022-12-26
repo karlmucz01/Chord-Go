9514420737d9606ef06a6999fb1543f54ad9d541
karlmucz01 <karl.mucz@gmail.com>
2022-12-26 00:35:38 -0500

    added some struct definitions

--- chord.go
+++ chord.go
@@ -4,21 +4,42 @@ import (
 	"crypto/sha1"
 	"encoding/hex"
 	"fmt"
-	"os"
+
+	// "os"
 	"strconv"
 )
 
+// defining constants
 const FT_SIZE = 16
 const UINT64_MAX = 18446744073709551615
 
+// definition for a chord node interface
+type Chord_Node interface {
+	find_successor(id uint64) *node
+	closest_preceding_node(id uint64) *node
+	create()
+}
+
+// definition for a chord node struct
+type node struct {
+	id           uint64
+	successor    *node
+	predecessor  *node
+	finger_table map[uint64]*node
+}
+
 func main() {
-	var id uint64 = get_hash(os.Args[1])
-	fmt.Println("SHA1 Hash: ", id)
+	var self node
+	self.id = get_hash("node1-self")
+	self.successor = &self
+	self.predecessor = &self
+	fmt.Println(self)
 }
 
 func get_hash(message string) uint64 {
 	var full_hash [20]byte = sha1.Sum([]byte(message))            // creating the hash byte array from the passed string
 	var truncated_hash string = hex.EncodeToString(full_hash[:8]) // getting first 8 bytes of a hash
+	fmt.Println(truncated_hash)
 	id, err := strconv.ParseUint(truncated_hash, 16, 64)
 	if err != nil {
 		fmt.Println("Error parsing node ID: ", err)
